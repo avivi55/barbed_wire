88d1a891c8d62a36d03ba11da2a168d1c51a086a
Caporal Winnie <52251710+avivi55@users.noreply.github.com>
2020-12-03 22:33:58 +0100

    Add files via upload

---
+++ barbed_wire/lua/entities/barbed_wire/shared.lua
@@ -0,0 +1,8 @@
+ENT.Type 			= "anim"
+ENT.Base 			= "base_gmodentity"
+ENT.Category        = "Winnie's Entity"
+ENT.PrintName		= "Barbed Wire"
+ENT.Author			= "Winnie"
+ENT.Contact         = "github.com/avivi55"
+ENT.Spawnable 		= true
+ENT.Purpose         = "Hurt People"
